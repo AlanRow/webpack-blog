cf0994f55f87c61e65656de8c671a94a3f2b289e
AlanRo <alekroga@gmail.com>
2022-07-26 22:35:34 +0500

    Tutorial: assets (styles, images, fonts, csv/xml)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import _ from "lodash"
+import './style.css'
+import Icon from './fox.jpeg'
+import Data from './data.xml';
+// import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');  
+  
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  console.log(Data);
+
+  import('./data.csv').then(
+    Notes => console.log(Notes)
+  )
+
   return element;
 }
 
