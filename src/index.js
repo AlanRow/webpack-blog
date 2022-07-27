d503586d43f3b3de04a3d3dfa8e04858a343df06
AlanRo <alekroga@gmail.com>
2022-07-27 23:46:19 +0500

    Tutorial: global assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import _ from "lodash"
 import './style.css'
 import Icon from './fox.jpeg'
-import Data from './data.xml';
+import Data from './data/data.xml';
 // import Notes from './data.csv';
+import toml from './data/data.toml';
+import yaml from './data/data.yaml';
+import json from './data/data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
@@ -15,7 +27,7 @@ function component() {
 
   console.log(Data);
 
-  import('./data.csv').then(
+  import('./data/data.csv').then(
     Notes => console.log(Notes)
   )
 
